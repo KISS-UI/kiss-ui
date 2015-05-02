9506c5c963fd880b4fcd006246c700b5e7f52ed9
Austin Bonander <austin.bonander@gmail.com>
2015-05-02 16:05:32 -0700

    Add close button to button test dialog

--- examples/button_test.rs
+++ examples/button_test.rs
@@ -2,7 +2,7 @@ extern crate kiss_ui;
 
 use kiss_ui::button::Button;
 use kiss_ui::container::Horizontal;
-use kiss_ui::callback::OnClick;
+use kiss_ui::callback::{OnClick, CallbackStatus};
 use kiss_ui::dialog::Dialog;
 use kiss_ui::text::Label;
 
@@ -13,10 +13,15 @@ fn main() {
                 Button::new()
                     .set_label(Some("Click me!"))
                     .set_onclick(show_new_dialog) 
+            )
+            .add_child(
+                Button::new()
+                    .set_label(Some("Close"))
+                    .set_onclick(close_dialog)
             );
         }))
         .set_title("Button test!")
-        .set_size_pixels(280, 140)
+        .set_size_pixels(140, 70)
     );
 }
 
@@ -27,4 +32,9 @@ fn show_new_dialog(_: Button) {
         .set_title("Button clicked!")
         .set_size_pixels(180, 90)
         .show();
+}
+
+fn close_dialog(_: Button) {
+    println!("Closing dialog!");
+    CallbackStatus::Close.set();
 }
