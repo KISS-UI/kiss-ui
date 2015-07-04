57bd99e520a4ddeebbb946f7bba80d7e6d5eba0f
Austin Bonander <austin.bonander@gmail.com>
2015-07-03 20:56:12 -0700

    Update .gitignore and tweak script

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,9 @@
+echo "Cleaning build..."
+cargo clean
+echo "Merging latest changes from master..."
 git merge master
-cargo doc
-rm -rf doc/
-cp -avr target/doc/ doc/
+mkdir docs/
+echo "Building API Documentation..."
+sh build_api_docs.sh
+echo "Building KISS-UI Users Guide..."
+sh build_guide.sh
